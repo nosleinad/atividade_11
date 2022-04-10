0dc736f8c2a84db0716527a0d1cebf2c301b7a69
Danielson Maciel <nosleinadjk@hotmail.com.br>
2022-04-10 04:30:28 +0044

    Criação de novas funcionalidades

--- script/script.js
+++ script/script.js
@@ -5,9 +5,28 @@ if (localStorage.getItem('listaPaciente') != null) {
     listaPaciente = JSON.parse(localStorage.getItem('listaPaciente'));
 }
 
+let cor = {
+    "Pré-Operatório": "warning",
+    "Transferido": "info",
+    "Em Cirurgia": "danger",
+    "Em Recuperação": "success",
+
+}
+
 function gravar() {
     let paciente = {};
-    paciente.nome = document.getElementById('nome').value;
+    paciente.nome = document.getElementById('nome').value.toUpperCase();
+    if (paciente.nome.length < 3) {
+        alert('Obrigatório informar um nome.');
+        return;
+    }
+
+    paciente.status = document.getElementById('status').value;
+    paciente.local = document.getElementById('local').value;
+    paciente.hora01 = document.getElementById('hora01').value;
+    paciente.hora02 = document.getElementById('hora02').value;
+    paciente.hora03 = document.getElementById('hora03').value;
+    paciente.hora04 = document.getElementById('hora04').value;
 
     let i = document.getElementById('id').value;
 
@@ -26,7 +45,23 @@ function gravar() {
 function carregarTable() {
     let tabela = '';
     for (i in listaPaciente) {
-        tabela += `<tr onclick='editar(${i})'><td>${listaPaciente[i].nome}</td></tr>`;
+
+        let status = listaPaciente[i].status;
+        let local = (listaPaciente[i].local);
+        let statusLocal = status;
+
+        if (local != '') {
+            statusLocal = `${status} (${local})`
+        }
+
+        tabela += `<tr onclick='editar(${i})'>
+                     <td>${listaPaciente[i].nome}</td>
+                    <td class='${cor[status]}'>${statusLocal}</td>
+                    <td>${listaPaciente[i].hora01}</td>
+                    <td>${listaPaciente[i].hora02}</td>
+                    <td>${listaPaciente[i].hora03}</td>
+                    <td>${listaPaciente[i].hora04}</td>
+                </tr>`;
     }
     document.getElementById('tbCorpo').innerHTML = tabela;
 }
@@ -34,12 +69,25 @@ function carregarTable() {
 function editar(i) {
     document.getElementById('id').value = i;
     document.getElementById('nome').value = listaPaciente[i].nome;
+    document.getElementById('status').value = listaPaciente[i].status;
+    document.getElementById('local').value = listaPaciente[i].local;
+    document.getElementById('hora01').value = listaPaciente[i].hora01;
+    document.getElementById('hora02').value = listaPaciente[i].hora02;
+    document.getElementById('hora03').value = listaPaciente[i].hora03;
+    document.getElementById('hora04').value = listaPaciente[i].hora04;
 }
+
 
 function novo() {
     document.getElementById('id').value = '';
     document.getElementById('nome').value = '';
-    
+    document.getElementById('status').value = '';
+    document.getElementById('local').value = '';
+    document.getElementById('hora01').value = '';
+    document.getElementById('hora02').value = '';
+    document.getElementById('hora03').value = '';
+    document.getElementById('hora04').value = '';
+
 }
 
 function apagar() {
@@ -48,7 +96,7 @@ function apagar() {
     localStorage.setItem('listaPaciente', JSON.stringify(listaPaciente));
     carregarTable();
     novo();
-    
+
 }
 
 carregarTable();
